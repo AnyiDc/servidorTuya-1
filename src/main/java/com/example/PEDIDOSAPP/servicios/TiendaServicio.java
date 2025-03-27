e80b7aa0c416df5907975021e17c9f02c3f75d44
yomapg <yoma0639@gmail.com>
2025-03-27 12:44:19 -0500

    se agregan consultas basicas de jpa

--- src/main/java/com/example/PEDIDOSAPP/servicios/TiendaServicio.java
+++ src/main/java/com/example/PEDIDOSAPP/servicios/TiendaServicio.java
@@ -1,7 +1,7 @@
 package com.example.PEDIDOSAPP.servicios;
 
+import com.example.PEDIDOSAPP.modelos.Usuario;
 import com.example.PEDIDOSAPP.repositorios.ITiendaRepositorio;
-import com.example.PEDIDOSAPP.repositorios.IUsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +9,16 @@ import org.springframework.stereotype.Service;
 public class TiendaServicio {
     @Autowired
     ITiendaRepositorio repositorio;
+    //Listar los metodos que activaran las consultas en la base de datos
+    //Guardar
+
+    //Buscar todos los registros
+
+    //Buscar por ID
+
+    //Modificar por ID
+
+    //Eliminar por ID
+
+
 }
--- src/main/java/com/example/PEDIDOSAPP/servicios/UsuarioServicio.java
+++ src/main/java/com/example/PEDIDOSAPP/servicios/UsuarioServicio.java
@@ -1,11 +1,81 @@
 package com.example.PEDIDOSAPP.servicios;
 
+import com.example.PEDIDOSAPP.modelos.Usuario;
 import com.example.PEDIDOSAPP.repositorios.IUsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioServicio {
     @Autowired
     IUsuarioRepositorio repositorio;
+
+    //Listar los metodos que activaran las consultas en la base de datos
+    //Guardar
+    public Usuario guardarUsuario(Usuario datosUsuario)throws Exception {
+        try {
+            //validar los datos de entrada
+           return this.repositorio.save(datosUsuario);
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+    //Buscar todos los registros
+    public List<Usuario> buscarTodosUsuarios()throws Exception{
+        try {
+            return this.repositorio.findAll();
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    //Buscar por ID
+    public Usuario buscarUsuarioPorId (Integer idUsuario)throws Exception{
+        try {
+            Optional<Usuario> usuarioBuscado=this.repositorio.findById(idUsuario);
+            if (usuarioBuscado.isPresent()){
+                return usuarioBuscado.get();
+            }else {
+                throw new Exception("el usuario consultado no esta en la BD");
+            }
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    //Modificar por ID
+    public Usuario modificarUsuario (Integer idUsuario,Usuario datosUsuario) throws Exception{
+        try {
+            Optional<Usuario> usuarioBuscado=this.repositorio.findById(idUsuario);
+           if (usuarioBuscado.isPresent()){
+               usuarioBuscado.get().setTelefono(datosUsuario.getTelefono());
+               usuarioBuscado.get().setCorreoElectronico(datosUsuario.getCorreoElectronico());
+               return this.repositorio.save(usuarioBuscado.get());
+           }else{
+               throw new Exception("Usuario no encontrado");
+           }
+
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    //Eliminar por ID
+    public boolean eliminarUsuario(Integer id)throws Exception{
+        try {
+            Optional<Usuario> usuarioBuscado=this.repositorio.findById(id);
+            if (usuarioBuscado.isPresent()){
+                this.repositorio.deleteById(id);
+                return true;
+            }else {
+                throw new Exception("Usuario no encontrado");
+            }
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
 }
